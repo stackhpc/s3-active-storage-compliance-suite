0ed4fbcd70b2ffdaa8b3a711cbc425177280fe98
Scott Davidson <scott@stackhpc.com>
2022-10-20 16:34:03 +0100

    Use anon docker volume

--- scripts/run-minio.sh
+++ scripts/run-minio.sh
@@ -1,1 +1,2 @@
-exec docker run --rm  -p 9000:9000 -p 9001:9001 -v "$(pwd)/testdata:/data" minio/minio server data --console-address ":9001"
+# Use anon storage volume so that test data is removed when container is stopped
+exec docker run --rm -p 9000:9000 -p 9001:9001 -v :/data minio/minio server data --console-address ":9001"
