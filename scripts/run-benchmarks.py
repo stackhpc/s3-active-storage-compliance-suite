868819c99dd04bc54872b91d2def22e9bba8b0d6
Scott Davidson <scott@stackhpc.com>
2022-11-14 10:17:56 +0000

    Add no-proxy benchmark

--- scripts/run-benchmarks.py
+++ scripts/run-benchmarks.py
@@ -10,14 +10,20 @@ from tqdm import tqdm
 
 #List of running proxies to benchmark against each other
 PROXY_URLS = {
-    'proxy-1': 'http://localhost:8000',
-    'proxy-2': 'http://localhost:8001',
+    'active-storage': 'http://localhost:8000',
+    # 'proxy-2': 'http://localhost:8001',
 }
 
-S3_SOURCE = 'http://localhost:9000'
+# S3_SOURCE = 'http://localhost:9000'
+S3_SOURCE = 'http://127.0.0.1:9000'
 AUTH = ('minioadmin', 'minioadmin')
 BUCKET = Path('benchmark-data')
-s3_fs = s3fs.S3FileSystem(key=AUTH[0], secret=AUTH[1], client_kwargs={'endpoint_url': 'http://localhost:9000'})
+
+N_repeats = 100 # Number of repetitions to average over for each test
+# Ns = (1, 1, 1)
+Ns = (100, 1000, 2000) #Dimensions of square chunk sizes to benchmark
+
+s3_fs = s3fs.S3FileSystem(key=AUTH[0], secret=AUTH[1], client_kwargs={'endpoint_url': S3_SOURCE})
 
 #Make sure the S3 bucket
 try:
@@ -25,9 +31,15 @@ try:
 except FileExistsError:
     pass
 
-N_repeats = 3 # Number of repetitions to average over for each test
+def sum_without_proxy(request_data, filename):
+    """Fetches full file from S3 and performs sum locally"""
+    dtype = request_data['dtype']
+    with s3_fs.open(BUCKET / filename, "rb") as file:
+        arr = np.frombuffer(file.read(), dtype=dtype)
+        result = np.sum(arr, dtype=dtype)
+        return result
 
-Ns = (100, 1000, 5000)
+
 fig, axs = plt.subplots(figsize=(3.5*len(Ns), 6), ncols=len(Ns))
 for i, array_dim in enumerate(Ns):
 
@@ -51,6 +63,18 @@ for i, array_dim in enumerate(Ns):
     }
     
     timings = {}
+
+    #Check timings without proxy for a reference timescale
+    times = []
+    print('Starting baseline benchmark without proxy')
+    for n in tqdm(range(N_repeats)):
+        t_start = time.perf_counter()
+        sum_without_proxy(request_data, filename)
+        t_end = time.perf_counter()
+        times.append(t_end - t_start)
+    timings['no-active-storage'] = times
+
+    #Make use of proxy
     for name, url in PROXY_URLS.items():
         times = []
         print('Starting benchmarks for proxy running on', url)
@@ -64,13 +88,15 @@ for i, array_dim in enumerate(Ns):
         timings[name] = times
 
     #Create figure to display results
-    axs[i].boxplot([timings[url] for url in PROXY_URLS], vert=True, labels=timings.keys())
+    axs[i].boxplot(timings.values(), vert=True, labels=timings.keys(), whis=(0, 100))
     axs[i].set_title(f'Chunk size = {N_bytes:.2f} MB')
-    axs[i].set_ylabel('Response time (s)')
+    axs[i].set_ylabel('Average time to perform reduction (seconds)')
     axs[i].set_xticklabels(axs[i].get_xticklabels(), rotation=0)
+    axs[i].set_ylim((0, axs[i].get_ylim()[1]))
     print()
 
 #Save figure to disk
+fig.suptitle("Python S3 active storage proxy benchmarks")
 timestamp = datetime.datetime.now().strftime("%Y-%d-%m--%H:%M:%S")
 output_file = f'benchmark--{timestamp}.png'
 plt.tight_layout()
@@ -78,3 +104,5 @@ plt.savefig(output_file)
 
 #Remove benchmark data from S3
 s3_fs.rm(str(BUCKET), recursive=True)
+
+
