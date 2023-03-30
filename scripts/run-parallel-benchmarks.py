7a42d909792dd23073a4251ef38eeb4450d82ca7
Scott Davidson <scott@stackhpc.com>
2023-03-30 20:56:01 +0100

    Refine benchmarking script

--- scripts/run-parallel-benchmarks.py
+++ scripts/run-parallel-benchmarks.py
@@ -1,6 +1,7 @@
 
 import time
 import datetime
+import json
 import s3fs
 import requests
 import numpy as np
@@ -20,7 +21,7 @@ AUTH = ('minioadmin', 'minioadmin')
 BUCKET = Path('benchmark-data')
 
 # Dimensions of square chunk sizes to benchmark
-SQUARE_DIMS = range(100, 1100, 300)
+SQUARE_DIMS = range(100, 1100, 100)
 
 def sum_without_proxy(fs, filename, request_data, expected_ans):
     """Fetches full file from S3 and performs sum locally"""
@@ -42,11 +43,8 @@ def sum_with_proxy(proxy_url, request_data, expected_ans):
         raise Exception(f'No-proxy case returned incorrect answer: {result} (expected {expected_ans})')
     return
 
-def repeat(x, n):
-    for _ in range(n):
-        yield x
 
-def main():
+def main(workers, request_count):
 
     timings = {}
 
@@ -67,7 +65,7 @@ def main():
         # Generate and upload the test data
         filename = f'test-data.dat'
         X = np.random.rand(N, N).astype('float32')
-        N_bytes = X.itemsize * X.size / 1024**2
+        N_bytes = X.itemsize * X.size / 10**6
         with s3_fs.open(BUCKET / filename, 'wb') as file:
             file.write(X.tobytes())
 
@@ -83,15 +81,21 @@ def main():
         # Run no-proxy benchmark
         print('-> No proxy:')
         t_start = time.perf_counter()
-        with ProcessPoolExecutor(max_workers=WORKERS) as executor:
-            _ = list(tqdm(executor.map(sum_without_proxy, repeat(s3_fs, REQUESTS), repeat(filename, REQUESTS), repeat(request_data, REQUESTS), repeat(X.sum(), REQUESTS)), total=REQUESTS))
+        with ProcessPoolExecutor(max_workers=workers) as executor:
+            _ = list(tqdm(executor.map(
+                sum_without_proxy,
+                *zip(*[(s3_fs, filename, request_data, X.sum()) for _ in range(request_count)])
+            ), total=request_count))
         run_timings['no-proxy'] = time.perf_counter() - t_start
 
         for name, url in PROXY_URLS.items():
             print(f'-> {name}:')
             t_start = time.perf_counter()
-            with ProcessPoolExecutor(max_workers=WORKERS) as executor:
-                _ = list(tqdm(executor.map(sum_with_proxy, repeat(url, REQUESTS), repeat(request_data, REQUESTS), repeat(X.sum(), REQUESTS)), total=REQUESTS))
+            with ProcessPoolExecutor(max_workers=workers) as executor:
+                _ = list(tqdm(executor.map(
+                    sum_with_proxy, 
+                    *zip(*[(url, request_data, X.sum()) for _ in range(request_count)])
+                ), total=request_count))
             run_timings[name] = time.perf_counter() - t_start
 
         timings[N] = run_timings
@@ -101,29 +105,40 @@ def main():
 if __name__ == '__main__':
     
     # Make plot
-    WORKERS_AND_REQUEST = [(2, 10), (4, 10), (8, 10)]
-    fig, axs = plt.subplots(figsize=(8, 4*len(WORKERS_AND_REQUEST)), nrows=len(WORKERS_AND_REQUEST))
+    workers_and_requests = [(4, 200), (8, 400), (16, 800)]
+    fig, axs = plt.subplots(figsize=(9, 3.5*len(workers_and_requests)), nrows=len(workers_and_requests))
     fig.suptitle("S3 active storage proxy benchmarks (parallel requests)")
 
-    for ax, (WORKERS, REQUESTS) in zip(axs, WORKERS_AND_REQUEST):    
+    results = {}
+    for ax, (workers, request_count) in zip(axs, workers_and_requests):    
 
-        print(f'\n Starting benchmark run with {WORKERS} workers and {REQUESTS} requests')
+        print(f'\n Starting benchmark run with {workers} workers and {request_count} requests')
         print('--------------------------------------------------------\n')
-        results = main()
+        batch_results = main(workers, request_count)
 
         for type in ['no-proxy', *PROXY_URLS]:
-            xdata = [results[N]['chunk-size-MB'] for N in SQUARE_DIMS]
-            ydata = [results[N][type] for N in SQUARE_DIMS]
+            xdata = [batch_results[N]['chunk-size-MB'] for N in SQUARE_DIMS]
+            ydata = [batch_results[N][type] for N in SQUARE_DIMS]
             ax.scatter(xdata, ydata, label=type)
             ax.plot(xdata, ydata)
 
         ax.set_xlabel("Chunk size (MB)")
-        ax.set_ylabel(f"Time taken to handle {REQUESTS} total requests\nfrom {WORKERS} parallel workers (seconds)")
+        ax.set_ylabel(f"Time taken to handle {request_count} total requests\nfrom {workers} parallel workers (seconds)")
         ax.legend()
-        ax.set_title(f'Parallel workers: {WORKERS} & Total requests: {REQUESTS}')
+        ax.set_title(f'Parallel workers: {workers} & Total requests: {request_count}')
+
+        results[f'{workers}-{request_count}'] = batch_results
+    
+    # Make sure all subplots have same ylims
+    ymin = min([ax.get_ylim()[0] for ax in axs])
+    ymax = max([ax.get_ylim()[1] for ax in axs])
+    for ax in axs:
+        ax.set_ylim((ymin, ymax))
 
     # Save figure
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
     output_file = f'benchmark-parallel--{timestamp}.png'
+    with open(output_file.replace('.png', '.json'), 'w') as file:
+        file.write(json.dumps(results, indent=4))
     plt.tight_layout()
     plt.savefig(output_file)
