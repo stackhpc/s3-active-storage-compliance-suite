79d8dee17b220f7aa2750b79ab3a2cb32fc3722e
Mark Goddard <mark@stackhpc.com>
2023-07-11 14:24:40 +0100

    Black reformatting for root directory

--- scripts/run-parallel-benchmarks.py
+++ scripts/run-parallel-benchmarks.py
@@ -1,4 +1,3 @@
-
 import time
 import datetime
 import json
@@ -10,46 +9,52 @@ from pathlib import Path
 from tqdm import tqdm
 from concurrent.futures import ProcessPoolExecutor
 
-#List of running proxies to benchmark against each other
+# List of running proxies to benchmark against each other
 PROXY_URLS = {
-    'active-storage': 'http://localhost:8000',
+    "active-storage": "http://localhost:8000",
 }
 
 # Configure upstream object store details
-S3_SOURCE = 'http://localhost:9000'
-AUTH = ('minioadmin', 'minioadmin')
-BUCKET = Path('benchmark-data')
+S3_SOURCE = "http://localhost:9000"
+AUTH = ("minioadmin", "minioadmin")
+BUCKET = Path("benchmark-data")
 
 # Dimensions of square chunk sizes to benchmark
 SQUARE_DIMS = range(100, 1100, 100)
 
+
 def sum_without_proxy(fs, filename, request_data, expected_ans):
     """Fetches full file from S3 and performs sum locally"""
     with fs.open(BUCKET / filename, "rb") as file:
-        arr = np.frombuffer(file.read(), dtype=request_data['dtype'])
-        result = np.sum(arr, dtype=request_data['dtype'])
+        arr = np.frombuffer(file.read(), dtype=request_data["dtype"])
+        result = np.sum(arr, dtype=request_data["dtype"])
         if not np.isclose(result, expected_ans):
-            raise Exception(f'No-proxy case returned incorrect answer: {result} (expected {expected_ans})')
+            raise Exception(
+                f"No-proxy case returned incorrect answer: {result} (expected {expected_ans})"
+            )
     return
 
 
 def sum_with_proxy(proxy_url, request_data, expected_ans):
     """Requests sum result from active storage proxy"""
-    response = requests.post(proxy_url + '/v1/sum/', json=request_data, auth=AUTH)
+    response = requests.post(proxy_url + "/v1/sum/", json=request_data, auth=AUTH)
     if response.status_code != 200:
         raise Exception("Proxy request failed. Error message: " + response.text)
-    result = np.frombuffer(response.content, dtype=request_data['dtype'])[0]
+    result = np.frombuffer(response.content, dtype=request_data["dtype"])[0]
     if not np.isclose(result, expected_ans):
-        raise Exception(f'No-proxy case returned incorrect answer: {result} (expected {expected_ans})')
+        raise Exception(
+            f"No-proxy case returned incorrect answer: {result} (expected {expected_ans})"
+        )
     return
 
 
 def main(workers, request_count):
-
     timings = {}
 
     # Initialize S3 interation stuff
-    s3_fs = s3fs.S3FileSystem(key=AUTH[0], secret=AUTH[1], client_kwargs={'endpoint_url': S3_SOURCE})
+    s3_fs = s3fs.S3FileSystem(
+        key=AUTH[0], secret=AUTH[1], client_kwargs={"endpoint_url": S3_SOURCE}
+    )
 
     # Make sure the S3 bucket
     try:
@@ -58,77 +63,101 @@ def main(workers, request_count):
         pass
 
     for N in SQUARE_DIMS:
-
-        print(f'Starting {N}x{N} array benchmarks')
+        print(f"Starting {N}x{N} array benchmarks")
         run_timings = {}
 
         # Generate and upload the test data
-        filename = f'test-data.dat'
-        X = np.random.rand(N, N).astype('float32')
+        filename = f"test-data.dat"
+        X = np.random.rand(N, N).astype("float32")
         N_bytes = X.itemsize * X.size / 10**6
-        with s3_fs.open(BUCKET / filename, 'wb') as file:
+        with s3_fs.open(BUCKET / filename, "wb") as file:
             file.write(X.tobytes())
 
-        run_timings['chunk-size-MB'] = N_bytes
+        run_timings["chunk-size-MB"] = N_bytes
 
         request_data = {
-            'source': S3_SOURCE,
-            'bucket': str(BUCKET),
-            'object': filename,
-            'dtype': str(X.dtype),
+            "source": S3_SOURCE,
+            "bucket": str(BUCKET),
+            "object": filename,
+            "dtype": str(X.dtype),
         }
 
         # Run no-proxy benchmark
-        print('-> No proxy:')
+        print("-> No proxy:")
         t_start = time.perf_counter()
         with ProcessPoolExecutor(max_workers=workers) as executor:
-            _ = list(tqdm(executor.map(
-                sum_without_proxy,
-                *zip(*[(s3_fs, filename, request_data, X.sum()) for _ in range(request_count)])
-            ), total=request_count))
-        run_timings['no-proxy'] = time.perf_counter() - t_start
+            _ = list(
+                tqdm(
+                    executor.map(
+                        sum_without_proxy,
+                        *zip(
+                            *[
+                                (s3_fs, filename, request_data, X.sum())
+                                for _ in range(request_count)
+                            ]
+                        ),
+                    ),
+                    total=request_count,
+                )
+            )
+        run_timings["no-proxy"] = time.perf_counter() - t_start
 
         for name, url in PROXY_URLS.items():
-            print(f'-> {name}:')
+            print(f"-> {name}:")
             t_start = time.perf_counter()
             with ProcessPoolExecutor(max_workers=workers) as executor:
-                _ = list(tqdm(executor.map(
-                    sum_with_proxy, 
-                    *zip(*[(url, request_data, X.sum()) for _ in range(request_count)])
-                ), total=request_count))
+                _ = list(
+                    tqdm(
+                        executor.map(
+                            sum_with_proxy,
+                            *zip(
+                                *[
+                                    (url, request_data, X.sum())
+                                    for _ in range(request_count)
+                                ]
+                            ),
+                        ),
+                        total=request_count,
+                    )
+                )
             run_timings[name] = time.perf_counter() - t_start
 
         timings[N] = run_timings
 
     return timings
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
     # Make plot
     workers_and_requests = [(4, 200), (8, 400), (16, 800)]
-    fig, axs = plt.subplots(figsize=(9, 3.5*len(workers_and_requests)), nrows=len(workers_and_requests))
+    fig, axs = plt.subplots(
+        figsize=(9, 3.5 * len(workers_and_requests)), nrows=len(workers_and_requests)
+    )
     fig.suptitle("S3 active storage proxy benchmarks (parallel requests)")
 
     results = {}
-    for ax, (workers, request_count) in zip(axs, workers_and_requests):    
-
-        print(f'\n Starting benchmark run with {workers} workers and {request_count} requests')
-        print('--------------------------------------------------------\n')
+    for ax, (workers, request_count) in zip(axs, workers_and_requests):
+        print(
+            f"\n Starting benchmark run with {workers} workers and {request_count} requests"
+        )
+        print("--------------------------------------------------------\n")
         batch_results = main(workers, request_count)
 
-        for type in ['no-proxy', *PROXY_URLS]:
-            xdata = [batch_results[N]['chunk-size-MB'] for N in SQUARE_DIMS]
+        for type in ["no-proxy", *PROXY_URLS]:
+            xdata = [batch_results[N]["chunk-size-MB"] for N in SQUARE_DIMS]
             ydata = [batch_results[N][type] for N in SQUARE_DIMS]
             ax.scatter(xdata, ydata, label=type)
             ax.plot(xdata, ydata)
 
         ax.set_xlabel("Chunk size (MB)")
-        ax.set_ylabel(f"Time taken to handle {request_count} total requests\nfrom {workers} parallel workers (seconds)")
+        ax.set_ylabel(
+            f"Time taken to handle {request_count} total requests\nfrom {workers} parallel workers (seconds)"
+        )
         ax.legend()
-        ax.set_title(f'Parallel workers: {workers} & Total requests: {request_count}')
+        ax.set_title(f"Parallel workers: {workers} & Total requests: {request_count}")
 
-        results[f'{workers}-{request_count}'] = batch_results
-    
+        results[f"{workers}-{request_count}"] = batch_results
+
     # Make sure all subplots have same ylims
     ymin = min([ax.get_ylim()[0] for ax in axs])
     ymax = max([ax.get_ylim()[1] for ax in axs])
@@ -137,8 +166,8 @@ if __name__ == '__main__':
 
     # Save figure
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d--%H:%M:%S")
-    output_file = f'benchmark-parallel--{timestamp}.png'
-    with open(output_file.replace('.png', '.json'), 'w') as file:
+    output_file = f"benchmark-parallel--{timestamp}.png"
+    with open(output_file.replace(".png", ".json"), "w") as file:
         file.write(json.dumps(results, indent=4))
     plt.tight_layout()
     plt.savefig(output_file)
